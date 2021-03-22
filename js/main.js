e0521c587f31b642772155139111bf84634335c3
F1rman <ihorkharchyshyn@gmail.com>
2021-03-22 03:57:48 +0200

    html ended

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-var app = angular.module('app', ['ngMaterial', 'ngRoute']);
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "main.htm"
-    })
-    .when("/red", {
-        templateUrl : "pages/red.html"
-    })
-    .when("/green", {
-        templateUrl : "pages/green.html"
-    })
-    .when("/blue", {
-        templateUrl : "pages/blue.html"
-    });
-});
-app.controller('global', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-});
+$(document).ready(()=>{
+  console.log('hi');
+})
