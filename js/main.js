07b2ec6bfbab35174fe9cb27e380d66bcb34afe6
F1rman <ihorkharchyshyn@gmail.com>
2021-03-22 19:35:28 +0200

    end

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-$(document).ready(()=>{
-$('header .nav li').each((i,e)=>{
-  $(e).click(()=>{
-    $('header .nav li').removeClass('active')
-    $(e).addClass('active')
+$(document).ready(() => {
+  $('header .nav li').each((i, e) => {
+    $(e).click(() => {
+      $('header .nav li').removeClass('active')
+      $(e).addClass('active')
+    })
   })
-})
-  $('.slider').slick({
-});
+  $('.slider').slick({});
+  $('.burger').click(() => {
+    $('header .nav').toggleClass('active')
+  })
+  $('header .container .nav li').click(() => {
+    $('header .nav').removeClass('active')
+  })
 })
